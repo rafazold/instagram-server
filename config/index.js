f761620a9d8be5352dca43001e25d86e641ea375
Rafa <zoldan.rafael@gmail.com>
2019-12-05 20:56:04 +0200

    added config file - db, jwt, port

---
+++ config/index.js
@@ -0,0 +1,7 @@
+const config = {
+    mongoUri : process.env.MONGODB_URI || 'mongodb://heroku_z5wx928q:qhmr0u73dlq875hrci3vintl2h@ds349618.mlab.com:49618/heroku_z5wx928q',
+    port: process.env.PORT || 4000,
+    jwtSecret: process.env.JWT_SECRET || 'hyJhsdl76sflJujsl',
+};
+
+module.exports = config;
